b0c9e88dc0fbd4ec9acf347d0c826a6b8818007a
qigezongdui <110756008+qigezongdui@users.noreply.github.com>
2022-08-08 16:13:46 +0800

    Add files via upload

---
+++ 变量的作用域和生命周期（2022.8.8）.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//int b = 19;
+//printf("4:%d\n", b);
+//int main()
+//{
+//	printf("1:%d\n", b);
+//	/*{
+//		int a = 20;
+//		printf("%d", a);
+//	}
+//	printf("%d", a);*/
+//
+//	{
+//		printf("2:%d\n", b);
+//	}
+//	printf("3:%d\n", b);
+//}
+
+extern int c;
+int main()
+{
+	printf("%d", c);
+}
