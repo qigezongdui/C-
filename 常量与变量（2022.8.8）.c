28621dab332a56d91d58858f0e48142a9203129a
qigezongdui <110756008+qigezongdui@users.noreply.github.com>
2022-08-08 20:07:55 +0800

    Add files via upload

---
+++ 常量与变量（2022.8.8）.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 10;
+//	a = 20;
+//	printf("%d", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int n = 10;
+//	//int n = 10;
+//	n = 20;
+//	printf("%d", n);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int arr1[10] = { 0 };//数组的定义
+//	const int n = 10;
+//	int arr2[n] = { 0 };
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//  
+//	int a = M;
+//	printf("%d", a);
+//	return 0;
+//
+//}
+
+
+
+int main()
+{
+	enum grade{A=2,B=5,C=1};
+	enum grade grade1 = A;
+	//A = 7;
+	printf("%d\n", A);
+	printf("%d\n", B);
+	printf("%d\n", C);
+}
