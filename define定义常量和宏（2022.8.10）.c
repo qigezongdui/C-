56fffa9c37a218582c5b12103f76b6d3fcd1525f
qigezongdui <110756008+qigezongdui@users.noreply.github.com>
2022-08-10 17:54:33 +0800

    Add files via upload

---
+++ define定义常量和宏（2022.8.10）.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+//#define PI 4
+//int main()
+//{
+//	printf("%d", PI);
+//
+//	return 0;
+//}
+
+
+//
+#define ADD(x,y) (x+y)
+
+//int ADD(int x,int y)
+//{
+//	return x + y;
+//}
+
+
+
+int main()
+{
+	printf("%d", 4 * ADD(2, 3));
+	return 0;
+}
+
