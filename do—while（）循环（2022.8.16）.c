faf12ec7957ace041009f91a4da62aa59594ac82
qigezongdui <110756008+qigezongdui@users.noreply.github.com>
2022-08-17 16:51:51 +0800

    Add files via upload

---
+++ do—while（）循环（2022.8.16）.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		if(i==5)
+//		continue;
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+
+
